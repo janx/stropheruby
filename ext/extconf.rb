7c8b5b6f000a2cb6fed20f430e98487ca3392c8f
yong <yong@yong-laptop.(none)>
2009-03-06 23:41:59 -0500

    fix linking

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
+have_library("expat")
+have_library("resolv")
 $CFLAGS = "#{ENV['CFLAGS']} -Wall -O3 "
 if CONFIG["MAJOR"].to_i >= 1 && CONFIG["MINOR"].to_i >= 8
   $CFLAGS << " -DHAVE_DEFINE_ALLOC_FUNCTION"
