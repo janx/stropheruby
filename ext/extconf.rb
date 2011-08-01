76e1c2d3a2ecd3d4d120c4b8386775ed1ebb3463
Jan Xie <jan.h.xie@gmail.com>
2011-08-01 18:06:59 +0800

    fix compilation error on jruby

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 have_library("expat")
 have_library("resolv")
-$CFLAGS = "#{ENV['CFLAGS']} -Wall -O3 -g"
+$CFLAGS = "#{ENV['CFLAGS']} -Wall -O3 -g -fPIC"
 if CONFIG["MAJOR"].to_i >= 1 && CONFIG["MINOR"].to_i >= 8
   $CFLAGS << " -DHAVE_DEFINE_ALLOC_FUNCTION"
 end
